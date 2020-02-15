8595842aaa90eeb8bf29331274ef593347cb3c52
BlackBook <nickbrathwaite@Nicks-MacBook-Pro-4.local>
2020-02-14 16:24:23 -0800

    Blackbook first commit

---
+++ BlackBook/BlackBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Nic Brathwaite on 12/17/19.
+//  Copyright © 2019 Nic Brathwaite. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
